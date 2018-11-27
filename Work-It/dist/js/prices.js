dbd18935e21e4742dafa8f5763ada8b347911214
Romeo <romeo.bertoldo@gmail.com>
2018-11-27 20:16:39 +0100

    JS for prices

--- Work-It/dist/js/prices.js
+++ Work-It/dist/js/prices.js
@@ -1,11 +1,5 @@
-var bottone = document.getElementsByName("checkout_pay");
-	for(var i=0;i<bottone.length;i++){
-		bottone[i].onclick = caricaPagamento;
-		bottone[i].posizione = i+1;
-	}
-
 function caricaPagamento(e){
-	var directory = 'checkout_steps/checkout_pay_'+e.target.posizione+'.htm #main';
+	var directory = 'checkout_steps/'+e+'.htm #main';
 	 $( "#main" ).load(directory);
 }
 
@@ -17,4 +11,27 @@ function caricaSceltaPiano(e){
 function caricaMetodoPagamento(e){
 	var directory = 'checkout_steps/'+e+'.htm #divv';
 	 $( "#print_choose" ).load(directory);
+}
+
+function verificaPagamentoCarta(){
+	var intestatario = document.getElementById('cc-name123');
+	var intestatario_error = document.getElementById('control-name-card');
+	var codice = document.getElementById('cc-number123');
+	var codice_error = document.getElementById('control-code-card');
+	intestatario_error.innerHTML = "";
+	codice_error.innerHTML = "";
+	if(intestatario.value == ""){
+		intestatario_error.innerHTML = "* Inserire un intestatario";
+		return false;
+	}
+	if(codice.value == "" || codice.length<16){
+		codice_error.innerHTML = "* Inserire un codice carta (16 cifre)";
+		return false;
+	}
+	if(isNaN(codice.value)){
+		codice_error.innerHTML = "* Bisogna inserire un numero";
+		return false;
+	}
+	$("#centralModalSuccess").modal();
+	return true;
 }
