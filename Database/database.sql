72e1f91619eb2f5b9fe5c70035f901bae0c4d29d
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2018-12-13 00:02:01 +0100

    Fix db

--- Database/database.sql
+++ Database/database.sql
@@ -1,7 +1,7 @@
 /* ===================================================================
 -- Script to create database
 
--- Name Database : DropItDatabase
+-- Name Database : DropIt_DB
 -- Address : localhost: {PORT}
 
 -- Choose a common port for debugging 
@@ -11,15 +11,14 @@
 /* ===================================================================
 -- Create table user
 -- ================================================================ */
-begin;
 
 CREATE TABLE myuser(
-  email      	VARCHAR(45) PRIMARY KEY,
+  email      	  VARCHAR(45) PRIMARY KEY,
   password      VARCHAR(45) NOT NULL,
-  cf		    VARCHAR(15) NOT NULL,
+  cf		        VARCHAR(15) NOT NULL,
   name          VARCHAR(45) NOT NULL,
-  datebirth     Date NOT NULL,
-  cityBirth     VARCHAR(20) NOT NULL,
+  birthDate     Date NOT NULL,
+  birthCity     VARCHAR(20) NOT NULL,
   city          VARCHAR(20) NOT NULL,
   address       VARCHAR(30) NOT NULL,
   UNIQUE (cf)
@@ -32,11 +31,9 @@ CREATE TABLE myuser(
 CREATE TABLE myorder(
   id            SERIAL PRIMARY KEY,
   description   VARCHAR(50) NOT NULL,
-  dateorder     DATE NOT NULL,
+  orderDate     DATE NOT NULL,
   address       VARCHAR(30) NOT NULL,
-  status        NUMERIC(1) NOT NULL,
-
-  constraint reference_id foreign key (id)    references makes(id) DEFERRABLE	INITIALLY	DEFERRED 
+  status        INTEGER NOT NULL
   );
   
   /* ===================================================================
@@ -44,12 +41,12 @@ CREATE TABLE myorder(
 -- ================================================================ */
 
 CREATE TABLE makes(
-  id            NUMERIC(300) PRIMARY KEY,
+  id          INTEGER PRIMARY KEY,
   email		    VARCHAR(45) NOT NULL,
 
   constraint reference_email foreign key (email) references myuser(email) DEFERRABLE	INITIALLY	DEFERRED,
   constraint reference_id foreign key (id)    references myorder(id) DEFERRABLE	INITIALLY	DEFERRED 
   );
   
-  
-  commit;
+ALTER TABLE myorder add constraint reference_id foreign key (id) references makes(id)
+DEFERRABLE INITIALLY DEFERRED;
