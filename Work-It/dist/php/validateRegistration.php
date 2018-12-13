e8aee820a96b97f46ee0d303eb23814d8697ddae
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2018-12-13 02:00:19 +0100

    Try to fix it

--- Work-It/dist/php/validateRegistration.php
+++ Work-It/dist/php/validateRegistration.php
@@ -1,36 +1,44 @@
+<!DOCTYPE html>
 <html>
-    <head></head>
+    <head><meta charset="UTF-8"></head>
     <body>
 
         <?php
 
-            $dbconn = pg_connect("host=localhost port=5434 dbname=DropIT_DB user=postgres password=password") or die('Could not connect: ' . pg_last_error());
+            $dbconn = pg_connect("host=localhost port=49987 dbname=DropIt_DB user=postgres") or die('Could not connect: ' . pg_last_error());
             if(!(isset($_POST['registrationButton']))){
                 header("Location: ../../index.html");
             }
             else{
-                $email = $_POST['email'];
-                $q1="select * from user where email= $1";
+                $email = $_POST['orangeForm-email'];
+                /* $q1="select * from user where email= $1"; */
+                $select = "select *";
+                $from = "from myuser";
+                $where = "where email=$1";
+                $q1 = $select + $from + $where;
                 $result=pg_query_params($dbconn, $q1, array($email));
                 if($line=pg_fetch_array($result,null,PGSQL_ASSOC)){
                     //header("Location: alreadyRegistered.html");
                     echo "<h1> Sorry, you are already a registered user</h1>";
-                    /*<a href=../paginaLogin/login.html> Click here to login
-                    </a>";*/
+                    setcookie("err_signup", "err_signup", time() + 3600, "/",  0);
                 }
                 else{
-                    $nome=$_POST['name'];
-                    $cognome = $_POST['surname'];
-                    // ...
-                    $password = md5($_POST['password']);
-                    $q2="insert into user values ($1,$2,$3,$4,$5)";
-                    $data=pg_query_params($dbconn,$q2,array($email,$nome,$cognome,$cap,$password));
+                    $password = md5($_POST['orangeForm-passReg']);
+                    $cf = $_POST['orangeForm-codiceFiscale'];
+                    $name=$_POST['orangeForm-name'];
+                    $birthDate = $_POST['orangeForm-date'];
+                    $birthCity = $_POST['orangeForm-cittaNascita'];
+                    $comune = $_POST['orangeForm-comune'];
+                    $address = $_POST['orangeForm-address'];
+                    $q2="insert into user values ($1,$2,$3,$4,$5,$6,$7,$8)";
+                    $data=pg_query_params($dbconn,$q2,array($email,$password,$cf,$name,$birthDate,
+                                                            $birthCity,$comune,$address));
                     if($data){
                         //header("Location: registrationCompleted.html");
                         echo "<h1> Registration is completed. Start using the website <br/></h1>";
-                        echo "<a href=../Welcome.php?name=$nome> Premi qui
+                        /*echo "<a href=../Welcome.php?name=$nome> Premi qui
                         </a>
-                        per inziare ad utilizzare il sito web";
+                        per inziare ad utilizzare il sito web";*/
                     }
                 }
             }
