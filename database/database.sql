103b5b8bd7d50a42e08ab3c7767aa41fab8229e5
Romeo <romeo.bertoldo@gmail.com>
2018-12-17 16:21:06 +0100

    add values for shop

--- database/database.sql
+++ database/database.sql
@@ -58,12 +58,8 @@ DEFERRABLE INITIALLY DEFERRED;
 -- ================================================================ */
 
 CREATE TABLE shop(
-  email      	  VARCHAR(45) PRIMARY KEY,
-  password      VARCHAR(45) NOT NULL,
-  cf		        VARCHAR(16) NOT NULL,
-  name          VARCHAR(45) NOT NULL,
-  city          VARCHAR(20) NOT NULL,
-  UNIQUE (cf)
+  name          VARCHAR(45) PRIMARY KEY,
+  category      VARCHAR(40) NOT NULL
   );
 
   /* ===================================================================
@@ -82,3 +78,63 @@ CREATE TABLE driver(
   address       VARCHAR(30) NOT NULL,
   UNIQUE (cf)
   );
+
+  /* ===================================================================
+-- Insert shop services
+-- ================================================================ */
+insert into shop values('Zio Pagnotta','Alimenti');
+insert into shop values('Il Signore degli Agnelli','Alimenti');
+insert into shop values('Casa del brodo','Alimenti');
+insert into shop values('La sorgente del panino','Alimenti');
+insert into shop values('Sfizi di carne','Alimenti');
+insert into shop values('Porkhause','Alimenti');
+insert into shop values('Mago Gelo','Alimenti');
+insert into shop values('Te la do io la merenda','Alimenti');
+insert into shop values('Comunale San Rocco','Farmaci');
+insert into shop values('Comunale g.p. Adami','Farmaci');
+insert into shop values('Comunale Besenello','Farmaci');
+insert into shop values('Farmaceutico di Nogaredo','Farmaci');
+insert into shop values('Multiservizi Rovereto','Farmaci');
+insert into shop values('Cobelli s.a.s.','Farmaci');
+insert into shop values('Soave','Farmaci');
+insert into shop values('San Giorgio di Manuela','Farmaci');
+insert into shop values('Johnny Stockino','Abbigliamento');
+insert into shop values('Baciami stupido','Abbigliamento');
+insert into shop values('Malvestio','Abbigliamento');
+insert into shop values('Tanta roba!','Abbigliamento');
+insert into shop values('Re Erode','Abbigliamento');
+insert into shop values('Scacco matto','Abbigliamento');
+insert into shop values('Millepiedi','Abbigliamento');
+insert into shop values('Bidon la scarpa','Abbigliamento');
+insert into shop values('Riprese matrimonio','FotoVideo');
+insert into shop values('Foto di edifici','FotoVideo');
+insert into shop values('Foto di gruppo','FotoVideo');
+insert into shop values('Video amatoriale','FotoVideo');
+insert into shop values('Foto panoramica','FotoVideo');
+insert into shop values('Video sorveglianza','FotoVideo');
+insert into shop values('Video professionale','FotoVideo');
+insert into shop values('Biblioteca dei sogni','LibriRiviste');
+insert into shop values('Un mondo di libri','LibriRiviste');
+insert into shop values('Un mondo di storie','LibriRiviste');
+insert into shop values('Il senso delle parole','LibriRiviste');
+insert into shop values('Un mondo di parole ','LibriRiviste');
+insert into shop values('Lo scaffale letterario','LibriRiviste');
+insert into shop values('Comunale n. 3','LibriRiviste');
+insert into shop values('Angolo della cultura','LibriRiviste');
+insert into shop values('Carfizzi','PostaPacchi');
+insert into shop values('Melissa','PostaPacchi');
+insert into shop values('Acquarica Del Capo','PostaPacchi');
+insert into shop values('Cannole','PostaPacchi');
+insert into shop values('Castro','PostaPacchi');
+insert into shop values('Cavallino','PostaPacchi');
+insert into shop values('Corsano','PostaPacchi');
+insert into shop values('Cursi','PostaPacchi');
+insert into shop values('Carto PC','Cancelleria');
+insert into shop values('Ca.Li.Pso','Cancelleria');
+insert into shop values('Cartolandia','Cancelleria');
+insert into shop values('Il Cartalibro','Cancelleria');
+insert into shop values('Te la do io la busta','Cancelleria');
+insert into shop values('Il paradiso della carta','Cancelleria');
+insert into shop values('La boutique della carta','Cancelleria');
+insert into shop values('Cartomania','Cancelleria');
+insert into shop values('Altro','Altro');
