573f340f46397dfba1873cbea6e9cd8f495b78c4
Romeo <romeo.bertoldo@gmail.com>
2018-12-17 16:22:05 +0100

    add php for list shop

--- dist/php/getOrderList.php
+++ dist/php/getOrderList.php
@@ -5,7 +5,7 @@
     session_start();
     $email = $_SESSION['userid'];
     $pass  = $_SESSION['passid'];
-    $q2 = "select o.id,o.description,o.orderdate,o.address,o.status from myorder o join makes m on m.id = o.id join myuser u on u.email = m.email where u.email=$1 and u.password=$2";
+    $q2 = "select o.id,o.description,o.orderdate,o.address,o.status from myorder o join makes m on m.id = o.id join myuser u on u.email = m.email where u.email=$1 and u.password=$2 order by o.orderdate desc";
     $result=pg_query_params($dbconn,$q2,array($email,$pass));
 
     if($line=pg_fetch_all($result)){
