6c6d4b8734a4d2d9d499f5a31fd3df069cafec4d
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2018-12-17 12:13:38 +0100

    Fix change password

--- dist/php/changePassword.php
+++ dist/php/changePassword.php
@@ -10,14 +10,16 @@
     if($pass != $old_password)
         echo "false";
 
-    $new_password = md5($_POST['orangeForm-pass2Reg']);
-    /*$q1="select password from myuser where email= $1";
-    $result=pg_query_params($dbconn, $q1, array($email));
-    $line=pg_fetch_array($result,null,PGSQL_ASSOC);*/
+    else {
+        $new_password = md5($_POST['orangeForm-pass2Reg']);
+        /*$q1="select password from myuser where email= $1";
+        $result=pg_query_params($dbconn, $q1, array($email));
+        $line=pg_fetch_array($result,null,PGSQL_ASSOC);*/
 
-    $q2 = "update myuser set password=$4 where email=$1";
-    $result=pg_query_params($dbconn,$q2,array($email));
+        $q2 = "update myuser set password=$1 where email=$2";
+        $result=pg_query_params($dbconn,$q2,array($new_password, $email));
 
-    if($result)     echo "true";
-    else            echo "Error";    
+        if($result)     echo "true";
+        else            echo "Error";
+    }    
 ?>
