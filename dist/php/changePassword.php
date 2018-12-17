e65a619f1cd9cd7708e302a7e478e7c49a23fe2c
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2018-12-17 02:48:10 +0100

    Add change password

---
+++ dist/php/changePassword.php
@@ -0,0 +1,23 @@
+<?php
+            
+    $dbconn = pg_connect("host=localhost port=5432 dbname=DropItDatabase user=postgres password=postgres") or die('Could not connect: ' . pg_last_error());
+            
+    session_start();
+    $email = $_SESSION['userid'];
+    $pass  = $_SESSION['passid'];
+    $old_password = md5($_POST['orangeForm-passReg']);
+
+    if($pass != $old_password)
+        echo "false";
+
+    $new_password = md5($_POST['orangeForm-pass2Reg']);
+    /*$q1="select password from myuser where email= $1";
+    $result=pg_query_params($dbconn, $q1, array($email));
+    $line=pg_fetch_array($result,null,PGSQL_ASSOC);*/
+
+    $q2 = "update myuser set password=$4 where email=$1";
+    $result=pg_query_params($dbconn,$q2,array($email));
+
+    if($result)     echo "true";
+    else            echo "Error";    
+?>
