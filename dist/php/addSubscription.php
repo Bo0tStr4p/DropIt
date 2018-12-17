c21e596dd6c793e4402559b894265da6e4704066
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2018-12-18 00:54:08 +0100

    Add php for subscription

---
+++ dist/php/addSubscription.php
@@ -0,0 +1,15 @@
+<?php
+            
+    $dbconn = pg_connect("host=localhost port=5432 dbname=DropItDatabase user=postgres password=postgres") or die('Could not connect: ' . pg_last_error());
+            
+    session_start();
+    $email = $_SESSION['userid'];
+    $sub = $_POST['plan'];
+    $subDate = $_POST['subscriptionDate'];
+
+    $q2 = "update myuser set subscription=$1 subscriptionDate=$2 where email=$3";
+    $result=pg_query_params($dbconn,$q2,array($sub, $subDate, $email));
+
+    if($result)     echo "true";
+    else            echo "Error";    
+?>
